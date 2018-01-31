6583327b07f83032722c1743f4b8775a4c501201
mkjubran <mkjubran@github.com>
2018-01-31 19:33:22 +0000

    Ver8.2

--- convert_HMDB_bin.py
+++ convert_HMDB_bin.py
@@ -12,6 +12,8 @@ tf.app.flags.DEFINE_string('data_dir', os.path.abspath('/data0/Aaron/hmdb_8x8_w1
                            """Data directory""")
 tf.app.flags.DEFINE_string('out_dir', os.path.abspath('out_test'),
                            """Directory to write new bins""")
+tf.app.flags.DEFINE_string('prefix', 'rgb24_',
+                           """Filename prefix""")
 tf.app.flags.DEFINE_string('path_to_file_list', os.path.join(os.path.curdir, 'hmdbTrainTestlist', 'trainlist01.txt'),
                            """Path to list of filenames to parse (set to 'None' to train on everything in data_dir)""")
 
@@ -29,21 +31,20 @@ label_dict = {}
 with open(FLAGS.path_to_file_list) as c:
     for line in c:
         (key, val) = line.split()
-        key = key.split('.')[0]
+        key = FLAGS.prefix + key.split('.')[0]
         label_dict[key.lower()] = int(val) - 1
-
+        
 frame_count = {}
 for file in finput:
     file_header = os.path.basename(file).split('.')[0]
 
-    #get label index
-    val = [value for key, value in label_dict.items() if key.lower() in file_header.lower()]
-    
-    if not val:
+    if file_header.lower() not in label_dict.keys():
         continue
 
-    label_idx = int(*val)
-    print label_idx
+    #get label index
+    label_idx = int(label_dict[file_header.lower()])
+
+    print (label_idx)
 
     with open(file, 'rb') as f:
         # read rest of file
@@ -95,8 +96,8 @@ for file in finput:
 
         #label to bytes
         label = struct.pack('i', label_idx)
-        # frame_type == 'P' :
-        if True: 
+
+        if frame_type == 'P':
             with open(bin_file, 'ab') as b:
                 bytes = data[i+8:i+16]+label+data[i+16:i+17]+data[i+17:i+frame_bytes]
                 b.write(bytes)
